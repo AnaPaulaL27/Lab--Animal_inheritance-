298bec219169390e199c7f904ec9642494ceba63
Ana P <103422102+AnaPaulaL27@users.noreply.github.com>
2022-05-01 00:03:09 +0100

    Update Cat.java

--- java/Cat.java
+++ java/Cat.java
@@ -1,26 +1,14 @@
 public class Cat extends Animal{
 
-   // private String name;
-    //private int age;
-        //deal with it in animal entirely
-
-    //constructing cat object
+ 
     public Cat(String name, int age) {
-        // this.name = name;
-        //this.age = age;
+        
         super(name, age);
     }
 
-//getters
 
-//    public String getName() {
-//        return this.name;
-//    
 
-//    public int getAge() {
- //       return this.age;
-
- //   }
+   }
 
     public String meow(){
         return "meow";
